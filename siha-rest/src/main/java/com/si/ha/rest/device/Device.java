d5d03142299e2d98873fd3191b5fa767dcd08279
Ács Ádám <acsadam0404@gmail.com>
2014-08-16 18:37:22 +0200

    siha-rest projekt minden függőségtől megszabadítva

--- siha-rest/src/main/java/com/si/ha/rest/device/Device.java
+++ siha-rest/src/main/java/com/si/ha/rest/device/Device.java
@@ -1,8 +1,40 @@
 package com.si.ha.rest.device;
 
-import groovy.transform.EqualsAndHashCode;
+/* TODO http://json-schema.org/ kéne ehelyett (majd minden objektumra) és azt kell verziózni multiplatformra */
+public class Device {
+	private String name;
 
-@EqualsAndHashCode
-class Device {
-	String name
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Device other = (Device) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
 }
