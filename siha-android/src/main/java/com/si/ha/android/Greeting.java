936757a4aa4256e2c55108121130fbed1380aa81
Ács Ádám <acsadam0404@gmail.com>
2014-08-16 08:37:37 +0200

    initial android

---
+++ siha-android/src/main/java/com/si/ha/android/Greeting.java
@@ -0,0 +1,16 @@
+package com.si.ha.android;
+
+public class Greeting {
+
+    private String id;
+    private String content;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+}
